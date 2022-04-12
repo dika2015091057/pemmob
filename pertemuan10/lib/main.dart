d000527420e9a1311e4a12f8d6e7a1d0977230dc
dika_rama <kadekdikaramasuryadi@gmail.com>
2022-04-12 10:11:26 +0800

    pertemuan10_bagian2

--- pertemuan10/lib/main.dart
+++ pertemuan10/lib/main.dart
@@ -17,8 +17,14 @@ class _pertemuan10State extends State<pertemuan10> {
   int nomer = 1;
   int umur = 21;
   TextEditingController input1 = TextEditingController();
-  void bersihkan() {
-    input1.clear();
+  TextEditingController input2 = TextEditingController();
+  TextEditingController output1 = TextEditingController();
+
+  void tambahkan() {
+    int angka1 = int.parse(input1.text);
+    int angka2 = int.parse(input2.text);
+    int hasil = angka1 + angka2;
+    output1.text = hasil.toString();
   }
 
   @override
@@ -30,13 +36,24 @@ class _pertemuan10State extends State<pertemuan10> {
             TextField(
               controller: input1,
               decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: "masukkan nama anda"),
+                  border: OutlineInputBorder(), hintText: "masukkan angka 1"),
+            ),
+            TextField(
+              controller: input2,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "masukkan angka ke 2"),
             ),
             ElevatedButton(
                 onPressed: () {
-                  bersihkan();
+                  tambahkan();
                 },
-                child: Text('hapus'))
+                child: Text('tambah')),
+            TextField(
+              controller: output1,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: "hasil penjumlahan"),
+            ),
           ],
         ),
       ),
